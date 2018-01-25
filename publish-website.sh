754e759cd94b5d042224a3e4a24628611506bb74
Adam Lopez <alopez@inf.ed.ac.uk>
2018-01-25 08:10:30 +0000

    Update publication script

--- publish-website.sh
+++ publish-website.sh
@@ -4,10 +4,20 @@ if [ "$1" == "" ]; then
     exit;
 fi
 
-# commit current directory
+# commit current content
 git add .
 git commit -m "$1"
+
+# generate new website
+jekyll build --config _config4inf.yaml
+
+# move to publishing directory
 pushd .
 cd ../nlu
-ls
+
+# publish new website
+git commit -m "$1"
+git cvsexportcommit -w ../web/teaching/courses/nlu -u -p -c HEAD^ HEAD
+
+# return to working directory
 popd
